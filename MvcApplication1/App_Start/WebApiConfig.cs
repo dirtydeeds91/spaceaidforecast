4787d4d8f0fc5537b3b0ebd39796175aa78cd29b
Ivan Mladenov <ivanmladenov91@gmail.com>
2014-11-12 20:46:44 +0200

    cors

--- MvcApplication1/App_Start/WebApiConfig.cs
+++ MvcApplication1/App_Start/WebApiConfig.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace MvcApplication1
 {
     public static class WebApiConfig
     {
+        [EnableCors(origins: "http://spaceaidproject.eu", headers: "*", methods: "*")]
         public static void Register(HttpConfiguration config)
         {
+            config.EnableCors();
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",

82362e9339071ed58229bfa7b09d3c2262e3ba20
Ivan Mladenov <ivanmladenov91@gmail.com>
2014-11-12 21:25:49 +0200

    new project with cors

---
+++ MvcApplication4/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace MvcApplication4
+{
+    public static class WebApiConfig
+    {
+        [EnableCors(origins: "http://spaceaidproject.eu", headers: "*", methods: "*")]
+        public static void Register(HttpConfiguration config)
+        {
+            config.EnableCors();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
